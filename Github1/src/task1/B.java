dd1ac9c9ce4c10d32761309db2b4b67398c9cd68
LENOVO <LENOVO@DESKTOP-IIT6LR1>
2022-04-25 13:18:43 +0530

    by sneha

---
+++ Github1/src/task1/B.java
@@ -0,0 +1,29 @@
+package task1;
+
+public class B {
+
+	
+
+		 public void m1()
+		 {
+			 System.out.println("hi m1");
+		 }
+		 public void m2()
+		 {
+			 System.out.println("hi m2");
+		 }
+		 
+		 public void m3()
+		 {
+			 System.out.println("hi m3");
+		 }
+		public static void main(String[] args) 
+		{
+			System.out.println("hi");
+			A a=new A();
+			a.m1();
+
+		}
+
+	}
+
